42796a9c8d6fd3b33c73861d496823493fd03c27
KauaPrado <kauaprado@live.com>
2026-04-23 23:51:55 -0300

    adicionando interface de repositorio

---
+++ EcommerceAPI/Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using EcommerceAPI.Domain.Entities;
+
+namespace EcommerceAPI.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product?> GetByIdAsync(Guid id);
+        Task AddAsync(Product product);
+    }
+}
